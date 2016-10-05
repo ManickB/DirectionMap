fdcc1716ad69749a7ce66b091550afd7a658479c
hari <jana.hariprasad@gmail.com>
2016-10-05 19:44:25 +0530

    Initial Commit

---
+++ DirectionMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DirectionMap
+//
+//  Created by ViVID on 10/5/16.
+//  Copyright © 2016 ViVID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DirectionMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DirectionMap
+//
+//  Created by ViVID on 10/5/16.
+//  Copyright © 2016 ViVID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
